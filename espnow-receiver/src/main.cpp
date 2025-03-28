3d5f79e085dda1a95e2b56f4ab5f8c58fa21dbe1
Paul Cager <paul.cager@gmail.com>
2025-03-28 19:08:56 +0000

    Allow receiver to have ethernet connection

--- espnow-receiver/src/main.cpp
+++ espnow-receiver/src/main.cpp
@@ -1,17 +1,51 @@
+#ifdef EVO_ETH
+#define ETH_PHY_TYPE ETH_PHY_DM9051
+#define ETH_PHY_ADDR         1
+#define ETH_PHY_CS           9
+#define ETH_PHY_IRQ          8
+#define ETH_PHY_RST          6
+#define ETH_PHY_SPI_HOST    SPI2_HOST
+#define ETH_PHY_SPI_SCK      7
+#define ETH_PHY_SPI_MISO     3
+#define ETH_PHY_SPI_MOSI    10
+#else
+#ifndef ETH_PHY_MDC
+#define ETH_PHY_TYPE ETH_PHY_LAN8720
+#if CONFIG_IDF_TARGET_ESP32
+#define ETH_PHY_ADDR  0
+#define ETH_PHY_MDC   23
+#define ETH_PHY_MDIO  18
+#define ETH_PHY_POWER -1
+#define ETH_CLK_MODE  ETH_CLOCK_GPIO0_IN
+#elif CONFIG_IDF_TARGET_ESP32P4
+#define ETH_PHY_ADDR  0
+#define ETH_PHY_MDC   31
+#define ETH_PHY_MDIO  52
+#define ETH_PHY_POWER 51
+#define ETH_CLK_MODE  EMAC_CLK_EXT_IN
+#endif
+#endif
+#endif
+
 #include <Arduino.h>
 
+#include "ETH.h"
+
 #include <esp_now.h>
+#include <esp_mac.h>
 #include <WiFi.h>
 #include <esp_wifi.h>
 #include <time.h>                       // time() ctime()
-#include <sys/time.h>                   // struct timeval
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
-#define MQTT_MAX_PACKET_SIZE 512
+#include <esp_http_server.h>
+
+
 #include <PubSubClient.h>
 #include <cQueue.h>
-#include "secrets.h"
+
+#include "../../secrets.h"
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 2
@@ -21,7 +55,7 @@
 // I could calculate the exact maximum size, but I can't be bothered.
 #define MAX_ESP_PACKET 255
 // Note that by default, the MQTT client limits messages to shorter than this.
-#define MESSAGE_LEN MQTT_MAX_PACKET_SIZE
+#define MESSAGE_LEN 512
 
 #define WIFI_WAIT_TIMEOUT_MILLIS 60000
 
@@ -43,8 +77,9 @@ typedef struct {
 
     unsigned long start_connect_time;
     unsigned long idle_until_time;
+    unsigned long last_send_time_millis;
     volatile uint32_t sequence;
-    uint32_t failed_mqqt_sends;
+    volatile uint32_t failed_mqtt_sends;
     volatile State state;
 
     Queue_t	q;
@@ -52,17 +87,30 @@ typedef struct {
 
 static state_t state = {};
 
+static uint8_t chipId[8];
+
+
 static WiFiClient wifi_client;
 static PubSubClient mqttClient(MQTT_BROKER, MQTT_PORT, wifi_client);
 // Define NTP Client to get time
 static WiFiUDP ntpUDP;
 static NTPClient timeClient(ntpUDP);
 
+static bool ethAvailable = false;      // True if MAC/PHY exist; does not need cable plugged in.
+static volatile bool ethConnected = false;
+
+#define HOSTNAME_PREFIX "espnow-receiver-"
+#define HOSTNAME_FORMAT (HOSTNAME_PREFIX "%02X%02X")
+static char host[sizeof(HOSTNAME_PREFIX) + 4 + 1];
+
 bool connect_to_mqtt();
 void send_to_mqtt(inbound_t *inbound_message);
 void disconnect_from_mqtt();
 
 void sanityCheck();
+static httpd_handle_t start_webserver();
+
+void send_queued_message();
 
 static void restart(const char *why) {
     Serial.println(why);
@@ -72,10 +120,13 @@ static void restart(const char *why) {
     ESP.restart();
 }
 
+// now() returns a string containing the current time in HH:MM:SS format. It is unreliable as
+// it depends on the timeClient, and so should only be used for debugging.
 const char *now() {
-    static char time_buff[9];
+    static char time_buff[16];
     if (!timeClient.isTimeSet()) {
-        return "";
+        sprintf(time_buff, "BOOT+%ld", millis() / 1000);
+        return time_buff;
     }
 
     time_t now = (time_t)timeClient.getEpochTime();
@@ -94,6 +145,43 @@ void wifi_event(arduino_event_id_t event) {
         Serial.println(WiFi.localIP());
     }
     last_event_time = millis();
+}
+
+void onEvent(arduino_event_id_t event) {
+    switch (event) {
+        case ARDUINO_EVENT_ETH_START:
+            // The hostname must be set after the interface is started, but needs
+            // to be set before DHCP, so set it from the event handler thread.
+
+            Serial.print("ETH Started: host=");
+            Serial.print(host);
+            Serial.print(", ETH_MAC=");
+            Serial.println(ETH.macAddress());
+            ETH.setHostname(host);
+            break;
+        case ARDUINO_EVENT_ETH_CONNECTED:
+            Serial.println("ETH Connected");
+            break;
+        case ARDUINO_EVENT_ETH_GOT_IP:
+            Serial.print("ETH Got IP: ");
+            Serial.println(ETH.localIP());
+            ethConnected = true;
+            break;
+//        case ARDUINO_EVENT_ETH_LOST_IP:
+//            Serial.println("ETH Lost IP");
+//            ethConnected = false;
+//            break;
+        case ARDUINO_EVENT_ETH_DISCONNECTED:
+            Serial.println("ETH Disconnected");
+            ethConnected = false;
+            break;
+        case ARDUINO_EVENT_ETH_STOP:
+            Serial.println("ETH Stopped");
+            ethConnected = false;
+            ethAvailable = false;
+            break;
+        default: break;
+    }
 }
 
 // callback function that will be executed when data is received
@@ -120,41 +208,57 @@ static void start_espnow(){
         restart("Error initializing ESP-NOW");
     }
 
-    esp_now_register_recv_cb(OnDataRecv);
+    // void (*)(const uint8_t*, const uint8_t*, int)'
+    // {aka 'void (*)(const unsigned char*, const unsigned char*, int)'} to
+    // 'esp_now_recv_cb_t'
+    // {aka 'void (*)(const esp_now_recv_info*, const unsigned char*, int)'}
+    esp_now_register_recv_cb(&OnDataRecv);
     esp_wifi_set_channel(WIFI_CHANNEL, WIFI_SECOND_CHAN_NONE);
 }
 
 void setup() {
+#ifndef ARDUINO_WT32_ETH01
+    // The Seeed S3s get quite hot. Reduce clock speed to keep cooler.
+    //setCpuFrequencyMhz(80);
+#else
+    setCpuFrequencyMhz(80);
+#endif
+
+    enableLoopWDT();
+
     // Initialize Serial Monitor
     Serial.begin(115200);
-
-    // The Seeed S3s get quite hot. Reduce clock speed to keep cooler.
-    setCpuFrequencyMhz(80);
 
     q_init(&state.q, sizeof(inbound_t), 3, FIFO, true);
 
     pinMode(LED_BUILTIN, OUTPUT);
 
-    configTime(0, 0, "pool.ntp.org", "2.uk.pool.ntp.org", "3.europe.pool.ntp.org");
+    esp_efuse_mac_get_default(chipId);
+    snprintf(host, sizeof(host),  HOSTNAME_FORMAT, chipId[4], chipId[5]);
 
-    // Set device as a Wi-Fi Station
+    mqttClient.setBufferSize(MESSAGE_LEN + MQTT_MAX_HEADER_SIZE + 2 + sizeof(MQTT_TOPIC) + 1 + 6);
+
+    //WiFi.onEvent(wifi_event);
+    WiFi.onEvent(onEvent);
+
+    Serial.println("ETH.begin():");
+    ethAvailable = ETH.begin();
+    Serial.print("ETH.begin returned:");
+    Serial.println(ethAvailable);
+
+    // Set device as a Wi-Fi Station so it can receive espnpw
     WiFi.mode(WIFI_MODE_STA);
-
     // Uncomment the following to add debugging of wireless timings.
     // A rule of thumb is that STA_CONNECT takes about 2.5 seconds, STA_GOT_IP about 1.5.
-    WiFi.onEvent(wifi_event);
+    //sanityCheck();
 
-    sanityCheck();
+    configTime(0, 0, "pool.ntp.org", "2.uk.pool.ntp.org", "3.europe.pool.ntp.org");
     timeClient.begin();
 
     start_espnow();
+    start_webserver();
 
-    // Start by initiating a WiFi connection, to start updating timeClient.
-    // Note that if the connection attempt fails we will reboot (which is probably sensible).
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    state.start_connect_time = millis();
-    last_event_time = millis();
-    state.state = state_t::waiting_for_wifi_connection;
+    state.state = state_t ::waiting_for_esp_now;
 }
 
 void sanityCheck() {
@@ -194,12 +298,23 @@ void loop() {
 
     if (!have_displayed && millis() > display_time) {
         // Delay printing of startup messages until we've been up for a few seconds and
-        // the serial connection is ready (especially for ESP32-S3).
+        // the serial connection is ready (especially for ESP32-S3). We may also have the timeClient
+        // available by then (for wired connections).
         have_displayed = true;
-        Serial.print("MAC: ");
+        Serial.print("Starting up ");
+        Serial.println(ESP.getChipModel());
+        Serial.print("Host ");
+        Serial.println(host);
+        Serial.print("ethAvailable=");
+        Serial.println(ethAvailable);
+        Serial.print("STA MAC: ");
         Serial.println(WiFi.macAddress());
+        Serial.print("ETH MAC: ");
+        Serial.println(ETH.macAddress());
         Serial.print("CPU Freq: ");
         Serial.println(getCpuFrequencyMhz());
+        Serial.print("Time: ");
+        Serial.println(timeClient.getFormattedTime());
     }
 
     mqttClient.loop();
@@ -219,21 +334,12 @@ void loop() {
         return;
     }
 
-    if (state.state == state_t::got_wifi_connection || state.state == state_t::idle_wifi) {
+    if (ethConnected || state.state == state_t::got_wifi_connection || state.state == state_t::idle_wifi) {
         timeClient.update();
+        send_queued_message();
     }
 
     if (state.state == state_t::got_wifi_connection) {
-        if (connect_to_mqtt()) {
-            inbound_t msg;
-            while (q_pop(&state.q, &msg)) {
-                send_to_mqtt(&msg);
-            }
-            disconnect_from_mqtt();
-        } else {
-            state.failed_mqqt_sends++;
-        }
-
         state.idle_until_time = millis() + 50;
         state.state = state_t::idle_wifi;
         return;
@@ -248,7 +354,7 @@ void loop() {
         return;
     }
 
-    if (!q_isEmpty(&state.q)) {
+    if (!q_isEmpty(&state.q) && !ethConnected) {
         // Note that attempting to avoid DHCP only saves about a second, so it is not attempted.
 //        uint8_t bssid[] = {0x12, 0xD7, 0xB0, 0x4A, 0x43, 0xDE};
 //        WiFi.config(
@@ -266,6 +372,24 @@ void loop() {
 
     // A little delay to reduce power.
     delay(10);
+}
+
+// If there's a queued message make sure we are connected to MQTT, pop it and
+// send to MQTT. Only send one message at a time, so that we don't starve the
+// MQTT client loop function.
+void send_queued_message() {
+    if (q_isEmpty(&state.q)) {
+        return;
+    }
+
+    if (connect_to_mqtt()) {
+        inbound_t msg;
+        if (q_pop(&state.q, &msg)) {
+            send_to_mqtt(&msg);
+        }
+    } else {
+        state.failed_mqtt_sends++;
+    }
 }
 
 bool connect_to_mqtt(){
@@ -286,41 +410,121 @@ void disconnect_from_mqtt() {
     mqttClient.disconnect();
 }
 
+#define MAC_FORMAT "%02X:%02X:%02X:%02X:%02X:%02X"
+#define MAC_BYTES(MAC) (MAC[0]),(MAC[1]),(MAC[2]),(MAC[3]),(MAC[4]),(MAC[5])
+
 void send_to_mqtt(inbound_t *inbound_message) {
     static uint8_t output_buff[MESSAGE_LEN];
-    static char mac_buff[19];
-    memset(mac_buff, 0, sizeof mac_buff);
-    for (int i = 0; i < 6; i++) {
-        snprintf(mac_buff +(3*i), 4, "%02X:", inbound_message->mac[i]);
-    }
-    mac_buff[17] = '\0';
 
     unsigned long rtc = timeClient.getEpochTime();
 
-    // TODO - should check mqttClient.getBufferSize() here.
+    static char in_mac[6*2 + 5 + 1];
+    snprintf(in_mac, sizeof in_mac, MAC_FORMAT,MAC_BYTES(inbound_message->mac));
+    String relay_mac = ethConnected ? ETH.macAddress() : WiFi.macAddress();
+    int delay = ethConnected ? 0 : (int)(millis() - state.start_connect_time);
+
 
     int n = snprintf(reinterpret_cast<char *>(output_buff), sizeof(output_buff) - 1,
-                     "{ \"t\": %ld, \"seq\": %d, \"mac\": \"%s\", \"delay\": %d, \"mqtt_err\": %d, \"data\": %s  }\n",
+                     "{ \"t\": %ld, \"seq\": %d, \"mac\": \"%s\", \"relay\": \"%s\", \"delay\": %d, \"mqtt_err\": %d, \"data\": %s  }\n",
                      rtc,
                      inbound_message->sequence,
-                     mac_buff,
-                     (int)(millis() - state.start_connect_time),
-                     state.failed_mqqt_sends,
+                     in_mac,
+                     relay_mac.c_str(),
+                     delay,
+                     state.failed_mqtt_sends,
                      inbound_message->message);
 
     unsigned int output_len = n > sizeof(output_buff) ? sizeof(output_buff) : n;
-    char topic_name[strlen(MQTT_TOPIC) + 1 + strlen(mac_buff) + 1];
+    char topic_name[strlen(MQTT_TOPIC) + 1 + strlen(in_mac) + 1];
     strcpy(topic_name, MQTT_TOPIC);
     strcat(topic_name, "/");
-    strcat(topic_name, mac_buff);
+    strcat(topic_name, in_mac);
 
-    if (!mqttClient.publish(topic_name, output_buff, output_len, true)) {
-        state.failed_mqqt_sends++;
+    // TODO - should check mqttClient.getBufferSize() here.
+
+    if (mqttClient.publish(topic_name, output_buff, output_len, true)) {
+        state.last_send_time_millis = millis();
+    } else {
+        state.failed_mqtt_sends++;
         Serial.print("mqttClient.publish failed: state=");
-        Serial.println(mqttClient.state());
+        Serial.print(mqttClient.state());
+        Serial.print(", output_len=");
+        Serial.print(output_len);
+        Serial.print(", buffer_size=");
+        Serial.println(mqttClient.getBufferSize());
     }
 
     Serial.print(now());
     Serial.print(" out ");
     Serial.println(reinterpret_cast<const char *>(output_buff));
+}
+
+
+static esp_err_t metrics_get_handler(httpd_req_t *req) {
+    // TODO - check buffer overflow
+    static char metrics_buff[1024];
+
+    httpd_resp_set_type(req, "text/plain");
+
+    // TODO - counts for each sender.
+    snprintf(metrics_buff, sizeof metrics_buff - 1,
+             "# TYPE espnow_receiver_message_count_total counter\n"
+             "espnow_receiver_message_count_total{receiver=\"%s\"} %d\n",
+             host,
+             state.sequence);
+    httpd_resp_sendstr_chunk(req, metrics_buff);
+
+    snprintf(metrics_buff, sizeof metrics_buff - 1,
+             "# TYPE espnow_receiver_failed_mqtt_count_total counter\n"
+             "espnow_receiver_failed_mqtt_count_total{receiver=\"%s\"} %d\n",
+             host,
+             state.failed_mqtt_sends);
+    httpd_resp_sendstr_chunk(req, metrics_buff);
+
+    snprintf(metrics_buff, sizeof metrics_buff - 1,
+             "# TYPE espnow_receiver_uptime gauge\n"
+             "espnow_receiver_uptime{receiver=\"%s\"} %f\n",
+             host,
+             (double)millis() / 1000.0);
+    httpd_resp_sendstr_chunk(req, metrics_buff);
+
+    snprintf(metrics_buff, sizeof metrics_buff - 1,
+             "# TYPE espnow_receiver_message_age gauge\n"
+             "espnow_receiver_message_age{receiver=\"%s\"} %f\n",
+             host,
+             ((double)(millis() - state.last_send_time_millis)) / 1000.0);
+    httpd_resp_sendstr_chunk(req, metrics_buff);
+
+    snprintf(metrics_buff, sizeof metrics_buff - 1,
+             "# TYPE espnow_receiver_heap_free_bytes gauge\n"
+             "espnow_receiver_heap_free_bytes{receiver=\"%s\"} %u\n",
+             host,
+             esp_get_free_heap_size());
+    httpd_resp_sendstr_chunk(req, metrics_buff);
+
+    // Indicate sent all chunks.
+    httpd_resp_sendstr_chunk(req, NULL);
+    return ESP_OK;
+}
+
+static const httpd_uri_t metrics = {
+        .uri       = "/metrics",
+        .method    = HTTP_GET,
+        .handler   = metrics_get_handler,
+};
+
+static httpd_handle_t start_webserver()
+{
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.lru_purge_enable = true;
+
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    if (httpd_start(&server, &config) == ESP_OK) {
+        httpd_register_uri_handler(server, &metrics);
+        return server;
+    }
+
+    ESP_LOGI(TAG, "Error starting server!");
+    return NULL;
 }
--- espnow-temperature/src/main.cpp
+++ espnow-temperature/src/main.cpp
@@ -20,9 +20,18 @@
 
 static DHT dht(4, DHT22);
 
+#define RTC_MEMORY_ADDR 17
+
+#define NUM_RECEIVERS 2
+uint8_t receiver_macs[NUM_RECEIVERS][6] = {
+      //{0x30, 0xc9, 0x22, 0xef, 0x4a, 0x14},       // WiFi MAC on ESP32 Ethernet (w headers)
+        {0x78, 0x42, 0x1c, 0x6a, 0x2d, 0x14},       // WiFi MAC on ESP32 Ethernet (w headers)
+        {0x30, 0xc9, 0x22, 0xef, 0x42, 0xc4},       // WiFi MAC on ESP32 Ethernet (directly soldered).
+      //{0x84, 0xfc, 0xe6, 0x78, 0x23, 0xec},       // seeed esp32-s3 number 2
+};
 //uint8_t receiver_mac[6] = {0x34, 0x94, 0x54, 0x24, 0x95, 0xc8};     // esp32-receiver
 //uint8_t receiver_mac[6] = {0x30, 0x30, 0xf9, 0x18, 0x14, 0xf8};     // seeed esp32-s3 number 1
-uint8_t receiver_mac[6] = {0x84, 0xfc, 0xe6, 0x78, 0x23, 0xec};     // seeed esp32-s3 number 2
+//uint8_t receiver_mac[6] = {0x84, 0xfc, 0xe6, 0x78, 0x23, 0xec};     // seeed esp32-s3 number 2
 //uint8_t receiver_mac[6] = {0xdc, 0xa6, 0x32, 0xcc, 0xa6, 0x4d};     // pi wlan0
 //uint8_t receiver_mac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
@@ -32,9 +41,15 @@ typedef struct {
     uint32_t send_fail;
     unsigned long time_millis;
     uint32_t last_send_time_millis;
+    uint8_t  last_proxy_mac[6];
 } payload_t;
 
+static const int SUCCESS_SLEEP_SECONDS = 15 * 60;
+static const int FAIL_SLEEP_SECONDS = 1 * 60;
 static payload_t payload;
+
+static int receives_outstanding;
+static int successful_sends;
 
 static void send_message(float humidity, float temperature);
 
@@ -64,34 +79,47 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 #else
 void OnDataSent(uint8_t *mac_addr, uint8_t status) {
 #endif
-    //print_payload();
+    if (false) print_payload();
+
+    receives_outstanding--;
+
+    if (status == ESP_NOW_SEND_SUCCESS) {
+        successful_sends++;
+        memcpy(payload.last_proxy_mac, mac_addr, sizeof(payload.last_proxy_mac));
+    }
+
+    if (receives_outstanding > 0) {
+        return;
+    }
+
+    if (successful_sends > 0) {
+        payload.send_success++;
+    } else {
+        payload.send_fail++;
+    }
 
     payload.last_send_time_millis = millis() - payload.time_millis;
     payload.sequence++;
 
-    if (status == ESP_NOW_SEND_SUCCESS) {
-        payload.send_success++;
-    }
-    else {
-        payload.send_fail++;
-    }
+    ESP.rtcUserMemoryWrite(RTC_MEMORY_ADDR, (uint32_t *)&payload, sizeof(payload_t));
 
-    ESP.rtcUserMemoryWrite(17, (uint32_t *)&payload, sizeof(payload_t));
-
-    uint32_t sleep_time_secs;
-    if (status == ESP_NOW_SEND_SUCCESS) {
-        sleep_time_secs = 15 * 60;
+    uint64_t sleep_time_secs;
+    if (successful_sends) {
+        sleep_time_secs = SUCCESS_SLEEP_SECONDS;
     } else {
         // Bring the next time forwards, but delay by a random amount to prevent collisions.
-        sleep_time_secs = random(45, 120);
+        sleep_time_secs = random(FAIL_SLEEP_SECONDS/2, FAIL_SLEEP_SECONDS*3/2);
     }
 
-    Serial.print("Up: "); Serial.print(millis()); Serial.print(" millis. Sleeping "); Serial.print(sleep_time_secs); Serial.println(" secs");
+    Serial.print("Status: "); Serial.print(status);
+    Serial.print(", Up: "); Serial.print(millis());
+    Serial.print(" millis. Sleeping ");
+    Serial.print(sleep_time_secs); Serial.println(" secs");
     Serial.flush();
     digitalWrite(LED_BUILTIN, LOW);
 
     // Instant does not wait for WiFi chip to idle.
-    ESP.deepSleepInstant(sleep_time_secs * 1000000);
+    ESP.deepSleepInstant(sleep_time_secs * 1000000L);
 }
 
 void setup() {
@@ -105,10 +133,11 @@ void setup() {
     digitalWrite(LED_BUILTIN, HIGH);
 
     Serial.begin(115200);
-    Serial.println();
+//    delay(2000);
+//    Serial.println("Yes, serial is working.");
 
     if (ESP.getResetInfoPtr()->reason == REASON_DEEP_SLEEP_AWAKE) {
-        ESP.rtcUserMemoryRead(17, (uint32_t *)&payload, sizeof(payload_t));
+        ESP.rtcUserMemoryRead(RTC_MEMORY_ADDR, (uint32_t *)&payload, sizeof(payload_t));
     } else {
         memset(&payload, 0, sizeof(payload_t));
     }
@@ -144,7 +173,9 @@ void setup() {
         return;
     }
 #else
-    esp_now_add_peer(receiver_mac, ESP_NOW_ROLE_SLAVE, 10, NULL, 0);
+    for (int i = 0; i < NUM_RECEIVERS; i++) {
+        esp_now_add_peer(receiver_macs[i], ESP_NOW_ROLE_SLAVE, 10, NULL, 0);
+    }
 #endif
 
     float h;
@@ -165,8 +196,16 @@ void setup() {
             send_message(h, t);
             return;
         }
+
+#if defined(ESP8266)
+        yield(); // Handle WiFi / reset software watchdog
+#endif
+
         delay(20);
     }
+
+    // Could not read the sensor, so no message has been sent. Sleep and try later.
+    ESP.deepSleepInstant(120 * 1000000L);
 }
 
 void loop() {
@@ -185,7 +224,7 @@ static void send_message(float humidity, float temperature) {
              "\"success\": %d, "
              "\"fail\": %d, "
              "\"t\": %0.1f, "
-             "\"h\": %0.1f "
+             "\"h\": %0.1f"
              "}",
              payload.sequence,
              millis(),
@@ -194,6 +233,17 @@ static void send_message(float humidity, float temperature) {
              temperature,
              humidity
     );
-    printf("buff %s\n", buff);
-    esp_now_send(receiver_mac, (uint8_t *)buff, strlen(buff)+1);
+    printf("buff %s\r\n", buff);
+    receives_outstanding = NUM_RECEIVERS;
+    successful_sends = 0;
+    memset(payload.last_proxy_mac, 0xff, sizeof(payload.last_proxy_mac));
+
+    int size = strlen(buff) + 1;
+    for (int i = 0; i < NUM_RECEIVERS; i++) {
+        esp_now_send(receiver_macs[i], (uint8_t *) buff, size);
+
+#if defined(ESP8266)
+        yield(); // Handle WiFi / reset software watchdog
+#endif
+    }
 }
